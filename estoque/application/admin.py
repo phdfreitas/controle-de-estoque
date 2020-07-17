67454964e21d0a18f6543a286f004022cdb03c19
phdfreitas <pedro.hnq.fraga@gmail.com>
2020-07-17 07:14:55 -0300

    Customizando Painel de Administrador

--- estoque/application/admin.py
+++ estoque/application/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from . models import Produto
 
 
-admin.site.register(Produto)
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin): # Permite a customizar o painel de admin
+    list_display = ('nome', 'marca', 'quantidade', 'cadastrado', 'preco', 'status')
+    list_filter = ('status', 'marca', 'preco') # Habilita o filtro
+    date_hierarchy = 'cadastrado'
+    search_fields = ('nome', 'descricao') # Habilita a pesquisa dos campos no painel admin
+    prepopulated_fields = {'slug':('marca', 'nome')} # "Cria" o slug baseado na marca e no nome
--- estoque/application/models.py
+++ estoque/application/models.py
@@ -6,15 +6,18 @@ from django.contrib.auth.models import User
 class Produto(models.Model):
     STATUS = (('disponível', 'Disponível'), ('indisponível', 'Indisponível'))
     nome = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=100)
     marca = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100)
     descricao = models.TextField()
     quantidade = models.IntegerField()
     preco = models.CharField(max_length=6)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE) # Mostrará o usuário que fez o cadastro do produto
-    cadastrado = models.DateTimeField(auto_now_add=True) # Só é alterado no momento do cadastro do produto
+    cadastrado = models.DateTimeField(default=timezone.now) # Só é alterado no momento do cadastro do produto
     alterado = models.DateTimeField(auto_now=True) # Salvará a data e hora em que sofrer alguma alteração
     status = models.CharField(max_length=12, choices=STATUS, default='indisponível')
+
+    class Meta:
+        ordering = ('cadastrado',)
 
     def __str__(self):
         return self.nome
