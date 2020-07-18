d0ee9e74bb79fee31d417e56f324443f00f985cc
phdfreitas <pedro.hnq.fraga@gmail.com>
2020-07-18 08:11:58 -0300

    Cadastro de Produtos

--- estoque/application/urls.py
+++ estoque/application/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProdutoListView.as_view(), name='listarProdutos'),
+    path('novoProduto', views.ProdutoCreateView.as_view(), name='cadastrarProduto'),
     path('<slug:slug>', views.ProdutoDetailView.as_view(), name='detalharProduto'),
 ]
--- estoque/application/views.py
+++ estoque/application/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from . models import Produto
 
 
@@ -12,3 +13,9 @@ class ProdutoDetailView(DetailView):
     model = Produto
     template_name = 'produto/detalhar-produto.html'
     context_object_name = 'produto'
+
+
+class ProdutoCreateView(CreateView):
+    model = Produto
+    fields = '__all__'
+    template_name = 'produto/cadastrar-produto.html'
