29844ac43c2ec4a6e907e9bd9e9efc02733f7f2e
phdfreitas <pedro.hnq.fraga@gmail.com>
2020-07-21 13:01:01 -0300

    Layout Cadastro e Login

--- estoque/statics/js/script.js
+++ estoque/statics/js/script.js
@@ -7,7 +7,14 @@ function addBootstrapClass() {
         document.getElementById("id_quantidade"),
         document.getElementById("id_preco"),
         document.getElementById("id_usuario"),
-        document.getElementById("id_status")
+        document.getElementById("id_status"),
+        document.getElementById("id_username"),
+        document.getElementById("id_password"),
+        document.getElementById("id_first_name"),
+        document.getElementById("id_last_name"),
+        document.getElementById("id_email"),
+        document.getElementById("id_password1"),
+        document.getElementById("id_password2")
     ]
 
     for(let i = 0; i < campos.length; i++){
@@ -21,6 +28,6 @@ addBootstrapClass()
 
 function removendoEspacosEmBranco() {
     let text = document.getElementById("detalhe-produto-descricao")
-    text.value = text.value.trim()
+    if(text !== null) text.value = text.value.trim()
 }
 removendoEspacosEmBranco()
